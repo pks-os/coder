a8e7140affab981aae6376b7b5948ebd7382b338
Jon Ayers <jon@coder.com>
2024-09-24 01:21:16 +0000

    dbauthz

--- coderd/cryptokeys/keycache.go
+++ coderd/cryptokeys/keycache.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	"golang.org/x/xerrors"
+
+	"github.com/coder/coder/v2/coderd/database"
 )
 
 var ErrKeyNotFound = xerrors.New("key not found")
@@ -12,6 +14,6 @@ var ErrKeyInvalid = xerrors.New("key is invalid for use")
 
 // Keycache provides an abstraction for fetching signing keys.
 type Keycache interface {
-	Latest(ctx context.Context) ([]byte, error)
-	Version(ctx context.Context, sequence int32) ([]byte, error)
+	Latest(ctx context.Context) (database.CryptoKey, error)
+	Version(ctx context.Context, sequence int32) (database.CryptoKey, error)
 }
